3243901c96fae3e2370e488c5def4c139c7ea73d
Nathan Adeyemi <adeyemi.n@northeastern.edu>
2023-03-17 12:44:09 -0400

    Updates: 1. Modified the directory structure to include all scripts and function in a separate Code folder. 2. Moved any code in Multi-Objective....R pertaining to the test bench to Test_Bed_Optimization.R 3. Moved any code in Multi-Objective....R pertaining to the full simulation to Full_Sim_Optimization.R. 4. Multi-Objective....R: When printing algorithm status at the end of an iteration, Pareto set objective info in ordered by the objective metrics (e.g. order(metric_1,metric_2,....,metric_N)) 5. MOSA Functions.R: in noisyNonDominatedSorting, both solution comparisons (e.g. if s1 dominates s2? and if s2 dominates s1?) now occur before assigning dominator/dominatee lists to prevent order from affecting front rank assignment 6. Inv_V_Grid_Search.R: Exhaustive search to find the true Pareto set for the test bench problem. 7. Moved any code in Multi-Objective....R pertaining to the NSGA II optimization algorithm to Test_Bed_NSGA_II.R

--- Code/Full_Sim_Optimization.r
+++ Code/Full_Sim_Optimization.r
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 suppressMessages(source('.Rprofile'))
 
 
@@ -138,3 +139,37 @@ if (continue_previous) {
     )
   )
 }
+=======
+  unfinished_dir <- file.path('home','adeyemi.n','MH_Simulation','Inpatient Bed Allocation Optimization','Data','full_sim_paused_envr.rdata')
+  use_test_bench <- F
+  if(dir.exists(unfinished_dir)){
+    load(unfinished_dir)
+    continue_previous <- T
+  } else{
+    # Directory to Store MOSA Results -----------------------------------------
+    res_dir <- file.path(".","Data","Full Sim Results",gsub('-','_',Sys.Date()))
+    if(!dir.exists(res_dir)){
+      dir.create(res_dir)
+    }
+    res_dir <- file.path(res_dir,paste0('Trial_',length(list.files(res_dir))+1))
+    dir.create(res_dir)
+    source(file.path('Simulations', 'Minnesota MH Network Simulation.R'))
+    siteInfo <-
+      data.table(readRDS(
+        file.path('Simulations', 'Function Requirements', 'Rates5.rds')
+      ))
+    obj_function_list <-
+      grep(pattern = 'mh_',
+           x = lsf.str(),
+           value = T)
+    nVar <- length(siteInfo[,unique(Bed_Group)])
+    optim_type = rep('min', 3)
+    init_sol <- unlist(sapply(
+      X = copy(siteInfo)[!duplicated(Bed_Group)][, .N, by = Facility_name][, N],
+      FUN = function(i)
+        c(1, rep(x = 0, times = i - 1))))
+      warmup <- 30
+      sim_length <-  200
+      continue_previous <- F
+  }
+>>>>>>> e29de9b (Updates:)
--- Code/Test_Bed_Optimization.r
+++ Code/Test_Bed_Optimization.r
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 rm(list = ls())
 source('.Rprofile')
 source(
@@ -87,3 +88,35 @@ for(instance in seq(15)){
   # saveRDS(results, file.path(res_dir, paste0(size, '_Network_DD_PUSA_results.rds')))
   saveRDS(instance_df, file.path(res_dir, paste0(size, '_Network_DD_PUSA_dataframe.rds')))
   
+=======
+inverted_V_logical <- T
+use_test_bench <- T
+continue_previous <- F
+# Directory to Store MOSA Results -----------------------------------------
+res_dir <- file.path(".", "Data","Sample MOSA Results",gsub('-','_',Sys.Date()))
+if(!dir.exists(res_dir)){
+  dir.create(res_dir)
+}
+res_dir <- file.path(res_dir,paste0('Trial_',length(list.files(res_dir))+1))
+dir.create(res_dir)
+read_init <- T
+n_queues <- nVar <- 1
+jackson_envir <- new.env()
+optim_type <- c('max', 'min','min')
+if (read_init) {
+  starter_data <-
+    readRDS(file.path('Data', 'Medium Testing Initial Solution (4 queues).rds'))
+  queues_df <- starter_data$network_df
+  n_queues <- nVar <- queues_df[,.N]
+}
+sys.source(file.path('.','Code','Jackson Network Test Bench.R'),envir = jackson_envir)
+print(queues_df)
+obj_function_list <- 
+  grep(pattern = 'TB_',
+        x = lsf.str(),
+        value = T)
+init_sol <- c(1, rep(0, (nVar - 1)))
+sim_length <- 2500
+warmup <- 200
+init_sol <- c(1, rep(0, (nVar - 1)))
+>>>>>>> e29de9b (Updates:)
